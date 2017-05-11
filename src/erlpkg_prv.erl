504792ff858ef19adc3bd8eecc35382e59a60bec
Bikram Chatterjee <razorpeak@gmail.com>
2017-05-11 17:53:20 +0200

    first version

---
+++ src/erlpkg_prv.erl
@@ -0,0 +1,33 @@
+-module(erlpkg_prv).
+
+-export([init/1, do/1, format_error/1]).
+
+-define(PROVIDER, erlpkg).
+-define(DEPS, [app_discovery]).
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([
+            {name, ?PROVIDER},            % The 'user friendly' name of the task
+            {module, ?MODULE},            % The module implementation of the task
+            {bare, true},                 % The task can be run by the user, always true
+            {deps, ?DEPS},                % The list of dependencies
+            {example, "rebar3 erlpkg"}, % How to use the plugin
+            {opts, []},                   % list of options understood by the plugin
+            {short_desc, "MSI and RPM builder"},
+            {desc, "Windows MSI and Linux RPM installer packager for erlang"}
+    ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+    io:format("project_apps ~p~n", [rebar_state:project_apps(State)]),
+    {ok, State}.
+
+-spec format_error(any()) ->  iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
